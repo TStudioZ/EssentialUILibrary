2cc336dc8c36d02dba3b116e77b425564dca97ed
Tomáš Zahálka <zahalka.t@gmail.com>
2018-07-09 15:38:27 +0200

    Added LiveDataEvent.

---
+++ EssentialUILibrary/src/main/java/com/tstudioz/essentialuilibrary/viewmodel/LiveDataEvent.java
@@ -0,0 +1,39 @@
+package com.tstudioz.essentialuilibrary.viewmodel;
+
+public class LiveDataEvent<T> {
+
+    private final T content;
+    private boolean hasBeenHandled = false;
+
+    public LiveDataEvent(T content) {
+        this.content = content;
+    }
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    public T getContentIfNotHandled() {
+        if (hasBeenHandled) {
+            return null;
+        } else {
+            hasBeenHandled = true;
+            return content;
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    public T peekContent() {
+        return content;
+    }
+
+    /**
+     * Returns the content if it's not already been handled.
+     */
+    public T peekContentIfNotHandled() {
+        if (hasBeenHandled)
+            return null;
+        return content;
+    }
+}
